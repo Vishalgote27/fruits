71adecce011ae3150866f15507e7ce4749878deb
vishal gote <vishal27@gmail.com>
2024-04-17 18:09:39 +0530

    front added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 require("dotenv").config({ path: "./config/.env" })
 const mongoose = require("mongoose")
+const path = require("path")
 const { connectMongo } = require("./config/db")
 connectMongo()
 const cors = require("cors")
 const app = express()
 
+app.use(express.static(path.join(__dirname, "dist")))
 app.use(cors())
 app.use(express.json())
 
@@ -13,6 +15,12 @@ app.use("/users", require("./routes/usersRoutes"))
 app.use("/addcart", require("./routes/addProductRoutes"))
 app.use("/admin", require("./routes/adminRoutes"))
 app.use("/buy", require("./routes/buyProductRoutes"))
+
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
+
 
 mongoose.connection.once("open", () => {
     console.log("MONGO DB CONNECT");
